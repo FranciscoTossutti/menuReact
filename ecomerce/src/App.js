0d1bdfcbd88f24846856db30a04b70223d058e15
FranciscoTossutti <frantossu@gmail.com>
2022-06-06 20:39:23 -0300

    nav

---
+++ ecomerce/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './componentes/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <h1>Ecommerce</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecomerce/src/componentes/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../estilos/Menu.css';
+
+function Menu() {
+    return ( 
+        <nav className='nav-main'>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contactos</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu;
